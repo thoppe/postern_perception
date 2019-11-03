fc372244719ba1e840e82d3dbfa0c56543d66bfc
Travis Hoppe <travis.hoppe@gmail.com>
2019-11-03 15:52:44 -0500

    Better eyes

--- docs/mouse_capture.js
+++ docs/mouse_capture.js
@@ -1,7 +1,12 @@
+const max_vision_extent = 2;
+
 $( document ).ready(function(e) {
 
-    let n_cols = 8;
-    let n_rows = 5;
+    //let n_cols = 8;
+    //let n_rows = 5;
+
+    let n_cols = 11;
+    let n_rows = 8;
 
     let box = $(".horrorbox");
 
@@ -11,9 +16,8 @@ $( document ).ready(function(e) {
 	}
     }
 
-
     draw_all(e)
-    
+   
 });
 
 $(document).mousemove(function(e) {
@@ -29,43 +33,48 @@ function draw_all(e) {
     if (typeof window.x == 'undefined') window.x = 0;
     if (typeof window.y == 'undefined') window.y = 0;
 
-    $("#cursorX").text(window.x);
-    $("#cursorY").text(window.y);
+    //$("#cursorX").text(window.x);
+    //$("#cursorY").text(window.y);
 
     let boxes = $(".eye");
-    boxes.each(function() {draw_single($(this))});
+    let window_height = $(window).height();
+    let window_width = $(window).width();
+    
+    boxes.each(function() {
+	draw_single($(this), window_height, window_width);
+    });
 }
 
-function draw_single(box, report=false) {
-
+function draw_single(box, window_height, window_width) {
 
     // Find pixel offset from the center of the box
     let wx = window.x - box.offset().left - box.width()/2;
     let wy = -(window.y - box.offset().top - box.height()/2);
 
     // Get relative coordinates
-    wx /= $(window).width();
-    wy /= $(window).height();
+    wx /= window_width;
+    wy /= window_height;
 
     // Scale coordinates to match known images
     
     wx *= 10
     wy *= 10
 
-    wx = Math.max(-2, Math.min(wx, 2))
-    wy = Math.max(-2, Math.min(wy, 2))
+    wx = Math.max(-max_vision_extent, Math.min(wx, max_vision_extent))
+    wy = Math.max(-max_vision_extent, Math.min(wy, max_vision_extent))
 
     wx = Math.round(wx)
-    wy = Math.round(wy)
-    
+    wy = Math.round(wy)    
 
     let f_img = `eye/${wx}_${wy}.jpg`
     box.attr("src", f_img);
 
-    if(!report) return true;
+    /*
+    if(!report)
+	return true;
 
     $("#filename").text(f_img);
     $("#cursorXT").text(wx);
     $("#cursorYT").text(wy);
-
+    */
 }
