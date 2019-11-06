423ee6f65c7483dc67de3a5edacb57e98afe4b5d
Travis Hoppe <travis.hoppe@gmail.com>
2019-11-05 20:18:54 -0500

    Working pass to python

--- API.py
+++ API.py
@@ -2,18 +2,19 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from starlette.responses import Response
 from starlette.middleware.cors import CORSMiddleware
-import io
 import cv2
+import numpy as np
 
-#from P0_generate_samples import eyeGAN
-#G = eyeGAN()
+from P0_generate_samples import eyeGAN
+
+G = eyeGAN()
 
 app = FastAPI()
 
 app.add_middleware(
-    CORSMiddleware, allow_origins=["*"], allow_methods=["*"],
-    allow_headers=["*"]
+    CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"]
 )
+
 
 class Coordinate(BaseModel):
     a0: float
@@ -24,21 +25,29 @@ class Coordinate(BaseModel):
 def read_root():
     return "eyeGAN"
 
+
 @app.post("/render")
 def render(coord: Coordinate):
-    
-    f_img = 'sample.jpg'   
-    img = cv2.imread(f_img)
-    
+
+    # This can be configurable
+    f_img = "sample.jpg"
+
+    # Multiple images can be generated, here just serve one
+    imgs = G(f_img, [coord.a0], [coord.a1])
+
+    # Take on the first (and only!) image
+    img = imgs.reshape(*imgs.shape[1:])
+
+    # Image is served as floats from [-1, 1], convert to uint8
+    img += 1
+    img *= 127.5
+    img = img.astype(np.uint8)
+
+    # Encoded with cv2, fix colorspace
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    # Encode with fixed quality
     encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
     raw_bytes = cv2.imencode(".jpg", img, encode_param)[1].tostring()
-    return Response(raw_bytes, media_type='image/jpg')
-    
-    '''
-    f_img = 'sample.jpg'
-        
-    with open(f_img, 'rb') as FIN:
-        raw_bytes = FIN.read()
-        
-    return Response(raw_bytes, media_type='image/jpg')
-    '''
+
+    return Response(raw_bytes, media_type="image/jpg")
--- docs/P0_test_api.py
+++ docs/P0_test_api.py
@@ -1,32 +1,25 @@
 import requests, json
 import numpy as np
 from tqdm import tqdm
-    
-url = 'http://127.0.0.1:8000/'
+import cv2
+
+url = "http://127.0.0.1:8000/"
 r = requests.get(url)
-print(r.content.decode('utf-8'))
+print(r.content.decode("utf-8"))
+
 #######################################################
 
-url = 'http://127.0.0.1:8000/render'
-data = {'a0' : 0.0, 'a1':1.0}
-r = requests.post(url,json=data)
+url = "http://127.0.0.1:8000/render"
 
-raw_bytes = r.content
+for _ in tqdm(range(1000)):
+    a0, a1 = np.random.uniform(-5, 5, size=(2,))
+    data = {"a0": a0, "a1": a1}
+    r = requests.post(url, json=data)
 
-import cv2
-#img = cv2.imdecode(raw_bytes, 0)
-bytes_as_np_array = np.frombuffer(raw_bytes, dtype=np.uint8)
-img = cv2.imdecode(bytes_as_np_array, 1)
-print(img)
-cv2.imshow('image',img)
-cv2.waitKey(0)
-exit()
-raw_bytes = cv2.imencode(".jpg", img, encode_param)[1].tostring()
-print(r.status_code)
-print(r.content)
-#for _ in tqdm(range(10**5)):
-#    
-#    print(r.content)
+    raw_bytes = r.content
 
+    bytes_as_np_array = np.frombuffer(raw_bytes, dtype=np.uint8)
+    img = cv2.imdecode(bytes_as_np_array, 1)
 
-
+    cv2.imshow("image", img)
+    cv2.waitKey(1)
